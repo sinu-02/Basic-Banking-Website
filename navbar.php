56a118ef8b6309ee2c5dc46fcd2ab115bae0c765
Sinu Xavier <89690609+sinu-02@users.noreply.github.com>
2021-12-17 18:17:58 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,31 @@
+<!-- Navbar used in all pages  -->
+ <!-- navbar -->
+
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="banklogo.png" alt="CSB Bank" style="width: 70px;">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href=" index.php " style="">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about" style="">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services" style=""> Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact" style="">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- navbar ends -->
